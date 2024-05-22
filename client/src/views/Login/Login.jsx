257cc445869c2be69d150a1e67187bffeb66dfab
Ricardo Rico <rricoz.co@gmail.com>
2024-05-22 15:12:22 -0500

    modificando el login y el register

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,54 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Login.module.css';
-import NavBar from '../../components/NavBar/NavBar';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from "@coreui/react";
+import { FaUser, FaLock } from "react-icons/fa";
+import styles from "./Login.module.css";
 
 const Login = () => {
-    return (
-        <div className={styles.loginContainer}>
-            <NavBar />
-            <h2>Iniciar Sesión</h2>
-            <form className={styles.loginForm}>
-                <div className={styles.formGroup}>
-                    <label htmlFor="id" className={styles.label}></label>
-                    <input type="text" id="id" name="id" className={styles.input} placeholder="Número de Identificación" />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="password" className={styles.label}></label>
-                    <input type="password" id="password" name="password" className={styles.input} placeholder="Contraseña" />
-                </div>
-                <button type="submit" className={styles.loginButton}>Iniciar Sesión</button>
-            </form>
-            <p className={styles.registerLink}>¿No estás registrado? <Link to="/register" className={styles.cuenta}>Crea una cuenta</Link></p>
-<p className={styles.dashboardLink}>Haz clic aquí para ver el dashboard <Link to="/dashboard">Dashboard</Link></p>
-        </div>
-    );
-}
+  return (
+    <div className={styles.loginBackground}>
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={10}>
+            <CCardGroup className={styles.cardGroup}>
+              <CCard className={`p-4 ${styles.card}`}>
+                <CCardBody>
+                  <CForm>
+                    <h1 className={styles.loginTitle}>Iniciar Sesión</h1>
+                    <p className={styles.loginSubtitle}>Ingresa a tu cuenta</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText >
+                        <FaUser className={styles.smokyGray}/>
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Número de identificación"
+                        autoComplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <FaLock className={styles.smokyGray}/>
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Contraseña"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton
+                          color="primary"
+                          className={`px-3 ${styles.loginButton}`}
+                        >
+                          Iniciar Sesión
+                        </CButton>
+                      </CCol>
+                      <CCol xs={6} className="text-right">
+                        <button className={styles.forgotPassword}>
+                          ¿Olvidaste tu contraseña?
+                        </button>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className={`${styles.signupCard} ${styles.card}`}>
+                <CCardBody className="text-center">
+                  <div className={styles.centerVertically}>
+                    <h2>Regístrate</h2>
+                    <p>
+                      Si no tienes una cuenta, regístrate ahora y empieza a
+                      conectarte con los productores locales.
+                    </p>
+                    <Link to="/register" className={styles.registerButton}>
+                      ¡Regístrate Ahora!
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  );
+};
 
 export default Login;
-
-
-// Login.jsx
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// // import Dashboard from '../DashBoard/Sidebar'; // Importa el componente Dashboard
-// import styles from './Login.module.css';
-
-// const Login = () => {
-//     const handleLogin = () => {
-//         // Aquí deberías realizar la lógica de inicio de sesión
-//         // Luego redirigir al usuario al Dashboard después del inicio de sesión
-//         // Esto se puede hacer con useHistory o simplemente colocando el componente Dashboard aquí
-//     }
-
-//     return (
-//         <div>
-//             <h2>Inicio de Sesión</h2>
-//             <button onClick={handleLogin}>Iniciar Sesión</button>
-//             <p>¿No tienes una cuenta? <Link to="/register">Regístrate aquí</Link></p>
-//             <p className={styles.dashboardLink}>Haz clic aquí para ver el dashboard <Link to="/dashboard">Dashboard</Link></p>
-//         </div>
-//     );
-// }
-
-// export default Login;
--- client/src/views/Register/Register.jsx
+++ client/src/views/Register/Register.jsx
@@ -1,76 +1,103 @@
-// Register.jsx
-import React, { useState } from 'react';
-import styles from './Register.module.css';
-// import { useNavigSate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import NavBar from '../../components/NavBar/NavBar';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from "@coreui/react";
+import { FaUser, FaLock } from "react-icons/fa";
+import styles from "./Register.module.css";
 
 const Register = () => {
-    const [formData, setFormData] = useState({
-        id: '',
-        name: '',
-        surnames: '',
-        email: '',
-        password: '',
-        userType: 'productor', // Default to 'productor'
-    });
-
-    // const navigate = useNavigate();
-
-    const handleChange = (e) => {S
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aquí iría la lógica para manejar el registro del usuario, como una solicitud a una API
-        console.log(formData);
-        // Redirigir al usuario después del registro
-        navigate('/login');
-    };
-
-    return (
-        <div className={styles.registerContainer}>
-            <NavBar />
-            <h2>Crear una cuenta</h2>
-            <form className={styles.registerForm} onSubmit={handleSubmit}>
-                <div className={styles.formGroup}>
-                    <label htmlFor="id">Número de Identificación:</label>
-                    <input type="text" id="id" name="id" value={formData.id} onChange={handleChange} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="name">Nombre:</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="surnames">Apellidos:</label>
-                    <input type="text" id="surnames" name="surnames" value={formData.surnames} onChange={handleChange} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="email">Correo Electrónico:</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="password">Contraseña:</label>
-                    <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label htmlFor="userType">Tipo de Usuario:</label>
-                    <select id="userType" name="userType" value={formData.userType} onChange={handleChange}>
-                        <option value="productor">Productor</option>
-                        <option value="tendero">Tendero</option>
-                    </select>
-                </div>
-                <button type="submit" className={styles.registerButton}>Registrarse</button>
-            </form>
-            <p className={styles.loginLink}>¿Ya estás registrado? <Link to="/login" className={styles.inicia}>Inicia Sesión</Link></p>
-        </div>
-    );
-}
+  return (
+    <div className={styles.registerBackground}>
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={10}>
+            <CCardGroup className={styles.cardGroup}>
+              <CCard className={`p-4 ${styles.card}`}>
+                <CCardBody>
+                  <CForm>
+                    <h1 className={styles.registerTitle}>Registrate</h1>
+                    <p className={styles.registerSubtitle}>Crea una cuenta</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        C.C.
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Número de identificación"
+                        autoComplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <FaUser className={styles.smokyGray} />
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Nombre(s)"
+                        autoComplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <FaUser className={styles.smokyGray} />
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Apellidos"
+                        autoComplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        @
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Email"
+                        autoComplete="email"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <FaUser className={styles.smokyGray} />
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="Tipo de usuario"
+                        autoComplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <FaLock className={styles.smokyGray} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Contraseña"
+                        autoComplete="current-password"
+                      />
+                    </CInputGroup>
+                    <div className="d-grid">
+                      <CButton color="success">Crea tu cuenta</CButton>
+                    </div>
+                    <div>
+                      <p className={styles.loginLink}>¿Ya tienes cuenta? <Link to="/login" className={styles.login}>Inicia sesión</Link></p>
+                    </div>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  );
+};
 
 export default Register;
